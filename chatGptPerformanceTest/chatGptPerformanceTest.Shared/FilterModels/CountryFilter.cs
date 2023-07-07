68cbf3fc67c986c3a6bc91f42bc3ac2ba3db8f4e
ihorbandura <ihor.bandura@softserveinc.com>
2023-07-07 19:14:56 +0300

    UC#1 Unit test

--- chatGptPerformanceTest/chatGptPerformanceTest.Shared/FilterModels/CountryFilter.cs
+++ chatGptPerformanceTest/chatGptPerformanceTest.Shared/FilterModels/CountryFilter.cs
@@ -2,9 +2,33 @@
 {
     public class CountryFilter
     {
-        public string? CountryName;
-        public int? Population;
-        public string? SortType;
-        public int? NumberOfRecords;
+        private string? countryName;
+        private int? population;
+        private string? sortType;
+        private int? numberOfRecords;
+        
+        public string? CountryName
+        {
+            get => countryName;
+            set => countryName = value;
+        }
+
+        public int? Population
+        {
+            get => population;
+            set => population = value;
+        }
+
+        public string? SortType
+        {
+            get => sortType;
+            set => sortType = value;
+        }
+
+        public int? NumberOfRecords
+        {
+            get => numberOfRecords; 
+            set => numberOfRecords = value;
+        }
     }
 }
